cc2ffefe590ff8b3ec6447376b8c7c45702d6d1e
Steven Dee <git@wholezero.org>
2026-07-12 00:30:38 -0700

    nix-darwin: switch to 26.05

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,8 +2,8 @@
   description = "Example nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
